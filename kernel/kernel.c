86f066f03337caf8e26659b3263f7b30086987af
Vladyslav Moisieienkov <vladyslav.moisieienkov@gmail.com>
2019-11-22 01:00:48 +0100

    feat, add kmalloc and free for dynamic memory management with 'smart' algorithm WIP

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,7 @@
 #include "../cpu/timer.h"
 #include "../drivers/screen.h"
 #include "../libc/string.h"
+#include "../libc/mem.h"
 
 void main() {
     isr_install();
@@ -9,15 +10,34 @@ void main() {
     clear_screen();
 
     kprint("Kernel input\n"
-           "Type DIE to halt the CPU \n >");
+           "Type DIE to halt the CPU \n > ");
 }
 
 void user_input(char *input) {
     if (strcmp(input, "DIE") == 0) {
         kprint("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
+    } else if (strcmp(input, "AL") == 0) {
+        char *test = (char*) kmalloc(6*sizeof(char));
+        if (test == NULL) {
+            kprint("bad");
+        }
+        char h[] = "hello\n";
+        strcopy(test, h);
+        kprint(test);
+        free(test);
+        test = NULL;
+        test = (char*) kmalloc(5*sizeof(char));
+        char t[] = "test";
+        strcopy(test, t);
+        kprint(test);
+        free(test);
+        test = NULL;
+        test = (char*) kmalloc(5*sizeof(char));
+        char c[] = "capt";
+        strcopy(test, c);
+        kprint(test);
+        free(test);
     }
-    kprint("You said: ");
-    kprint(input);
-    kprint("\n> ");
+    kprint("\n > ");
 }
--- libc/mem.c
+++ libc/mem.c
@@ -12,3 +12,80 @@ void memory_set(u8 *dest, u8 val, u32 len) {
     u8 *temp = (u8 *)dest;
     for ( ; len != 0; len--) *temp++ = val;
 }
+
+// head of linkedlist
+u32 free_mem_addr = 0x10000;
+
+typedef struct memory_sector {
+    struct memory_sector *next;
+    u32 is_used;
+    u32 pages;
+} MEM_SEC;
+
+u32 PAGE_SIZE = 0x1000;
+u32 calculate_pages_num(u32 size) {
+    u32 pages_num = size / PAGE_SIZE;
+    if ((size % PAGE_SIZE) > 0)
+        pages_num++;
+    return pages_num;
+}
+
+MEM_SEC *find_empty_slot(u32 pages_num) {
+    MEM_SEC *p = (MEM_SEC*) free_mem_addr;
+    while (p != NULL) {
+        if (p->is_used == 0 && p->pages >= pages_num)
+            return p;
+        p = p->next;
+    }
+    return NULL;
+}
+
+MEM_SEC *find_last_slot() {
+    MEM_SEC *p = (MEM_SEC*) free_mem_addr;
+    while (p != NULL)
+        if (p->next == NULL)
+            return p;
+    return NULL;
+}
+
+void insert(u32 addr, u32 pages) {
+    MEM_SEC *new_slot = (MEM_SEC*) addr;
+    new_slot->is_used = 1;
+    new_slot->pages = pages;
+    new_slot->next = NULL;
+}
+
+void free(void *addr) {
+    MEM_SEC *p = (MEM_SEC*) free_mem_addr;
+    while (p != NULL) {
+        if (((u32) p + sizeof(MEM_SEC)) == (u32) addr) {
+            p->is_used = 0;
+            return;
+        }
+        p = p->next;
+    }
+}
+
+u32 kmalloc(u32 size) {
+    u32 pages_num = calculate_pages_num(size);
+
+    // it is a first time we allocate memory
+    if (free_mem_addr == 0x10000) {
+        free_mem_addr += 1;
+        MEM_SEC *msec = (MEM_SEC*) free_mem_addr;
+        msec->next = NULL;
+        msec->is_used = 1;
+        msec->pages = pages_num;
+        return (u32) msec + (u32) sizeof(MEM_SEC);
+    } else {
+        MEM_SEC *slot = find_empty_slot(pages_num);
+        if (slot == NULL) {
+            slot = find_last_slot();
+            u32 new_addr = (u32) slot + sizeof(MEM_SEC) + slot->pages * PAGE_SIZE;
+            insert(new_addr, pages_num);
+            return (u32) new_addr + (u32) sizeof(MEM_SEC) + (u32) pages_num * PAGE_SIZE;
+        }
+        slot->is_used = 1;
+        return (u32) slot + (u32) sizeof(MEM_SEC);
+    }
+}
